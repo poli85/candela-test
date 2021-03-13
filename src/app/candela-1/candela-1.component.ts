8e6956c0b2d5a93128590a503d7e638c8ab2bc25
claudio.poli <claudio.poli@infocube.it>
2021-03-13 14:58:53 +0100

    creato nuovo componente candela 1

---
+++ src/app/candela-1/candela-1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candela-1',
+  templateUrl: './candela-1.component.html',
+  styleUrls: ['./candela-1.component.css']
+})
+export class Candela-1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/candela-1/candela-1.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Candela-1Component } from './candela-1.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [Candela-1Component]
+})
+export class Candela-1Module { }
