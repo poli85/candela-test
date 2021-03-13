e044c728b8133b0bb25a513d7aeab6073341d6a5
claudio.poli <claudio.poli@infocube.it>
2021-03-13 15:06:03 +0100

    candela 2

---
+++ src/app/candela-2/candela-2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candela-2',
+  templateUrl: './candela-2.component.html',
+  styleUrls: ['./candela-2.component.css']
+})
+export class Candela-2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/candela-2/candela-2.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Candela-2Component } from './candela-2.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [Candela-2Component]
+})
+export class Candela-2Module { }
